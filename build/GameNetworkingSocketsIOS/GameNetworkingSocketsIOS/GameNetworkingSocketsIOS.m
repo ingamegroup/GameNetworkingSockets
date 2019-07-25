178870ada247780f54155239814a29f128868651
Levent Sevgili <leventahmetsevgili@gmail.com>
2019-07-25 12:37:17 +0300

    xcode project setup included. Compatible with xcode version.

---
+++ build/GameNetworkingSocketsIOS/GameNetworkingSocketsIOS/GameNetworkingSocketsIOS.m
@@ -0,0 +1,13 @@
+//
+//  GameNetworkingSocketsIOS.m
+//  GameNetworkingSocketsIOS
+//
+//  Created by levent ahmet sevgili on 21.07.2019.
+//  Copyright © 2019 levent ahmet sevgili. All rights reserved.
+//
+
+#import "GameNetworkingSocketsIOS.h"
+
+@implementation GameNetworkingSocketsIOS
+
+@end
